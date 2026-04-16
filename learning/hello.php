1e3494f01594f4574ba0a314cf1f16958f6b837f
Tung HOANG <tung.hoang@gmail.com>
2026-04-16 11:29:55 +0700

    Add 2 files

---
+++ learning/hello.php
@@ -0,0 +1,10 @@
+<?php
+  $friend="Jack Ma";
+  $age = 15 * 4;
+?>
+<html>
+  <body>
+    <div><span>Name:</span><span><?= $friend ?></span></div>
+    <div><span>Age:</span><span><?= $age ?></span></div>
+  </body>
+</html>
